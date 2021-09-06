35cb224f8c2040136c7cd2ec47268ec47b755467
nathan <nathan@olightworld.com>
2021-09-06 16:20:40 +0800

    firstcommit

---
+++ README.md
@@ -0,0 +1,17 @@
+## 说明
+
+```bash
+
+#准备需要替换翻译的excel数据,拷贝放在excel文件夹下的i18n文件的sheet1和sheeet2中
+
+#准备需要替换的Json文件,放在json文件夹下面
+
+# 安装读取excel依赖
+$ npm install node-xlsx --save
+
+# 执行脚本
+$ node index.js
+
+# 在json文件夹中生成的Json文件格式化,拷贝到需要的地方,大概能替换80%以上
+
+```
---
+++ index.js
@@ -0,0 +1,87 @@
+const fs = require("fs");
+const xlsx = require('node-xlsx')
+
+const i18n = 'i18n' // 修改成自己的文件名
+const sheets = xlsx.parse(`./excel/${i18n}.xlsx`)
+
+
+function selectData(data) {
+    if(typeof data !== 'object' || data === null){
+        throw new TypeError('传入参数不是对象')
+    }
+    const newData = {}, data1 = getExcel(), dataKeys = Object.keys(data);
+    dataKeys.forEach(value => {
+       const currentDataValue = data[value];
+       if (typeof currentDataValue !== "object" || currentDataValue === null) {
+           newData[value] = currentDataValue + '===缺失';
+           data1.arrList.forEach(item => {
+                if (currentDataValue === item.oldVal) {
+                    newData[value] = item.newVal
+                } else {
+                    if (currentDataValue && item.oldVal) {
+                        if (currentDataValue.toLowerCase() === (item.oldVal).toString().toLowerCase()) {
+                            newData[value] = item.newVal
+                        }
+                    }
+                }
+            })
+            data1.arrList1.forEach(item => {
+                if (currentDataValue === item.oldVal) {
+                    newData[value] = item.newVal
+                } else {
+                    if (currentDataValue && item.oldVal) {
+                        if (currentDataValue.toLowerCase() === (item.oldVal).toString().toLowerCase()) {
+                            newData[value] = item.newVal
+                        }
+                    }
+                }
+            })
+        } else { 
+           newData[value] = selectData(currentDataValue);
+        } 
+     }); 
+    return newData;
+}
+function getExcel () {
+    const arrList = [],sheetData = sheets[0]['data'], arrList1 = [],sheetData1 = sheets[1]['data'];
+    for(var idx in sheetData){
+        arrList.push({
+            index: idx,
+            oldVal: sheetData[idx][0],
+            newVal: sheetData[idx][1]
+        })
+    }
+    for(var idx in sheetData1){
+        arrList1.push({
+            index: idx,
+            oldVal: sheetData1[idx][0],
+            newVal: sheetData1[idx][1]
+        })
+    }
+    return {
+        arrList,
+        arrList1
+    }
+}
+function createJson (json) {
+    const buffer = JSON.stringify(json)
+    const filename = 'es-ES' // 修改成需要生成json的文件名
+    fs.writeFile(`./json/${filename}.json`, buffer, function(err) {
+        if (err) {
+            console.log("Write failed: " + err);
+            return;
+        }
+        console.log("Write completed.");
+    });
+}
+function getJson () {
+    fs.readFile('./json/en-CA.json','utf8',function (err, data) {
+        if (err) {
+            console.log("read failed: " + err);
+            return;
+        }
+        const json = JSON.parse(data);
+        createJson(selectData(json))
+    });
+}
+getJson()
